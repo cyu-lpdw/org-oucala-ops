7472c43c61e208779526cade2a4e94074b510f58
François Michaud <francois-xavier.michaud@cyu.fr>
2023-07-10 15:15:22 +0200

    Change directory structure

--- README.md
+++
@@ -1,4 +0,0 @@
-echo "# org-oucala-ops" >> README.md
-
-kubectl port-forward $(kubectl get pods --selector "app.kubernetes.io/name=traefik" --output=name) 9000:9000
-
--- install.sh
+++
@@ -1,7 +0,0 @@
-#helm repo add traefik https://helm.traefik.io/traefik
-helm repo update
-helm uninstall traefik || true
-helm upgrade --install \
-    traefik traefik/traefik \
-    -f traefik.values.yml  
-#kubectl apply -f dashboard.yaml
